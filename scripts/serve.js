e5478a408908cebbf242c580eef8fd4f8dbeb433
Dmitriy Alexandrov <newsmobileads@gmail.com>
2025-07-14 12:57:07 +0500

    test

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,19 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const {execSync} = require('child_process');
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
 
-const chokidar = require('chokidar'); 
-const express = require('express');
-const serveStatic = require('serve-static');
+const chokidar = require("chokidar");
+const express = require("express");
+const serveStatic = require("serve-static");
 
-const {parse, serialize} = require('parse5');
-const utils = require('parse5-utils');
+const { parse, serialize } = require("parse5");
+const utils = require("parse5-utils");
 
-const watcher = new chokidar.FSWatcher({ignored: '*.swp'});
-const glob = require('glob');
+const watcher = new chokidar.FSWatcher({ ignored: "*.swp" });
+const glob = require("glob");
 
-const {debounce} = require('lodash');
-const open = require('open');
+const { debounce } = require("lodash");
+const open = require("open");
 
 class Server {
   // read configuration
@@ -21,17 +21,17 @@ class Server {
     const {
       autoOpen = false,
 
-      watchPattern = 'docs/**/*',
+      watchPattern = "docs/**/*",
 
       port = 8000,
 
-      serveIndexes = ['index.html'],
-      serveDir = 'docs-html',
+      serveIndexes = ["index.html"],
+      serveDir = "docs-html",
       cacheControl = false,
 
-      ssePath = '/events',
-      sseEventName = 'reload',
-      sseEventMessage = 'rebuilt'
+      ssePath = "/events",
+      sseEventName = "reload",
+      sseEventMessage = "rebuilt",
     } = parameters;
 
     this.configs = {
@@ -61,16 +61,18 @@ events.addEventListener("${this.configs.sseEventName}", function(e) {
     this.configure();
   }
 
-  // configure server  
+  // configure server
   configure() {
     this.app = express();
-    this.app.use(serveStatic(this.configs.serveDir, {
-      index: this.configs.serveIndexes,
-      cacheControl: this.configs.cacheControl,
-    }));
+    this.app.use(
+      serveStatic(this.configs.serveDir, {
+        index: this.configs.serveIndexes,
+        cacheControl: this.configs.cacheControl,
+      }),
+    );
     this.app.get(this.configs.ssePath, this.sseRequestHandler.bind(this));
-    this.app.get('/', (req, res) => {
-      res.redirect('/ru/index.html');
+    this.app.get("/", (req, res) => {
+      res.redirect("/ru/index.html");
     });
     this.sseResponseMessage = `event: ${this.configs.sseEventName}\ndata: ${this.configs.sseEventMessage}\n\n`;
 
@@ -83,9 +85,9 @@ events.addEventListener("${this.configs.sseEventName}", function(e) {
   // handle sse request
   sseRequestHandler(req, res, next) {
     res.writeHead(200, {
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive'
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive",
     });
     res.flushHeaders();
 
@@ -93,38 +95,46 @@ events.addEventListener("${this.configs.sseEventName}", function(e) {
     // - rebuild documentation
     // - inject sse into documentation
     // - notify client with reload event
-    watcher.on('all', debounce((event, path) => {
-      console.info(`event: ${event}, path: ${path}`);
-      
-      try {
-        this.buildDocumentation.call(this);
-      } catch (err) {
-        console.error('failed building documentation:', err);
-      }
+    watcher.on(
+      "all",
+      debounce((event, path) => {
+        console.info(`event: ${event}, path: ${path}`);
 
-      this.injectSSE.call(this);
-      this.sendSSEReloadResponse.call(this, res);
-    }, 500));
+        try {
+          this.buildDocumentation.call(this);
+        } catch (err) {
+          console.error("failed building documentation:", err);
+        }
+
+        this.injectSSE.call(this);
+        this.sendSSEReloadResponse.call(this, res);
+      }, 500),
+    );
   }
 
   // build documentation
   buildDocumentation() {
-    console.info('building documentation');
+    console.info("building documentation");
 
-    execSync('npm run build:docs');
+    execSync("npm run build:docs");
   }
 
   injectSSE() {
-    console.info('injecting sse into html');
+    console.info("injecting sse into html");
 
-    const pattern = path.join(this.configs.serveDir, '**', '*.html');
-    const paths = glob.globSync(pattern, {ignore: 'node_modules/**'}).map(p => path.join(process.cwd(), p));
-  
+    const pattern = path.join(this.configs.serveDir, "**", "*.html");
+    const paths = glob
+      .globSync(pattern, { ignore: "node_modules/**" })
+      .map((p) => path.join(process.cwd(), p));
+
     for (const path of paths) {
       try {
-        const html = fs.readFileSync(path, 'utf8');
-        const transformed = this.injectSSEIntoHTML(html, this.configs.sseScript);
-        fs.writeFileSync(path, transformed, {encoding: 'utf8'});
+        const html = fs.readFileSync(path, "utf8");
+        const transformed = this.injectSSEIntoHTML(
+          html,
+          this.configs.sseScript,
+        );
+        fs.writeFileSync(path, transformed, { encoding: "utf8" });
       } catch (err) {
         process.exit(1);
       }
@@ -133,7 +143,7 @@ events.addEventListener("${this.configs.sseEventName}", function(e) {
 
   // send sse reload message
   sendSSEReloadResponse(res) {
-    console.info('sending sse event');
+    console.info("sending sse event");
 
     res.write(this.sseResponseMessage);
   }
@@ -142,8 +152,8 @@ events.addEventListener("${this.configs.sseEventName}", function(e) {
     const parsed = parse(html);
 
     traverse(parsed, (node) => {
-      if (node.nodeName === 'head') {
-        const sseScript = utils.createNode('script');
+      if (node.nodeName === "head") {
+        const sseScript = utils.createNode("script");
         const sseScriptBody = utils.createTextNode(sse);
 
         utils.append(sseScript, sseScriptBody);
@@ -158,20 +168,22 @@ events.addEventListener("${this.configs.sseEventName}", function(e) {
   listen() {
     this.app.listen(this.configs.port);
 
-    console.info(`serving on: http://0.0.0.0:${this.configs.port}/ru/index.html`);
+    console.info(
+      `serving on: http://0.0.0.0:${this.configs.port}/ru/index.html`,
+    );
 
     if (this.configs.autoOpen) {
-        open(`http://0.0.0.0:${this.configs.port}/ru/index.html`);
+      open(`http://0.0.0.0:${this.configs.port}/ru/index.html`);
     }
-}
+  }
 }
 
 function traverse(node, cb) {
   cb(node);
 
-  const childNodes = node['childNodes'];
+  const childNodes = node["childNodes"];
   if (!childNodes) {
-    return ;
+    return;
   }
 
   for (const childNode in childNodes) {
